9ad1fd2b3be555d355b9625f4d0bd1f3cc014da7
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2018-09-19 16:29:57 +0700

    fix build script

--- webpack.config.js
+++ webpack.config.js
@@ -5,36 +5,20 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 module.exports = {
 	mode: 'production',
 	entry: {
-		app: ["@babel/polyfill", './index.js'],
+		app: ['./index.js'],
 	},
 	output: {
 		library: 'beta',
 		libraryTarget: 'umd',
 		globalObject: "typeof self !== 'undefined' ? self : this",
 		path: path.resolve(__dirname, 'dist'),
-    filename: 'ajax.js'
+		filename: 'ajax.js',
 	},
-	/*optimization: {
-		splitChunks: {
-			chunks: "all",
-		},
+	optimization: {
 		minimizer: [
 			new UglifyJsPlugin({
-				test: /\.js($|\?)/i
-			})
+				test: /\.js($|\?)/i,
+			}),
 		],
-	},*/
-	module: {
-		rules: [{
-		  type: 'javascript/auto',
-			test: /\.mjs$/,
-			use: [],
-		}, {
-			test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-      },
-    },]
 	},
 }
