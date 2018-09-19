918a03cb417cd92e06f1238c82c6b1f1371d081c
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2018-09-19 16:29:18 +0700

    convert to oldstyle js

--- src/ajax.js
+++ src/ajax.js
@@ -1,171 +1,199 @@
 var querystring = require('querystring')
 
-function get(base, path) {
-	return new Request().setMethod('GET').setBase(base).setPath(path)
+function get (base, path) {
+	return newRequest()
+		.setMethod('GET')
+		.setBase(base)
+		.setPath(path)
 }
 
-function post(base, path) {
-	return new Request().setMethod('POST').setBase(base).setPath(path)
+function post (base, path) {
+	return newRequest()
+		.setMethod('POST')
+		.setBase(base)
+		.setPath(path)
 }
 
-function del(base, path) {
-	return new Request().setMethod('DELETE').setBase(base).setPath(path)
+function del (base, path) {
+	return newRequest()
+		.setMethod('DELETE')
+		.setBase(base)
+		.setPath(path)
 }
 
-function patch(base, path) {
-	return new Request().setMethod('PATCH').setBase(base).setPath(path)
+function patch (base, path) {
+	return newRequest()
+		.setMethod('PATCH')
+		.setBase(base)
+		.setPath(path)
 }
 
-function head(base, path) {
-	return new Request().setMethod('HEAD').setBase(base).setPath(path)
+function head (base, path) {
+	return newRequest()
+		.setMethod('HEAD')
+		.setBase(base)
+		.setPath(path)
 }
 
-class Request {
-	constructor() {
-		this.parse = _ => _
-		this.hooks = []
-		this.base = ''
-		this.path = ''
-		this.credentials = 'same-origin'
-		this.query = ''
+function asis (data) {
+	return data
+}
+
+function newRequest () {
+	var r = {
+		parse: asis,
+		hooks: [],
+		base: '',
+		path: '',
+		credentials: 'same-origin',
+		query: '',
 	}
 
-	merge(obj) {
+	r.merge = function (obj) {
 		return Object.assign(this.clone(), obj)
 	}
 
-	clone() {
-		return Object.assign(new Request(), this, {hooks: this.hooks.slice()})
+	r.clone = function () {
+		return Object.assign(newRequest(), this, { hooks: this.hooks.slice() })
 	}
 
-	setQuery(query) {
+	r.setQuery = function (query) {
 		if (!query) return this
-		return this.merge({query: "?" + serializeQuery(query)})
+		return this.merge({ query: '?' + serializeQuery(query) })
 	}
 
-	setCredentials(credentials) {
-		return this.merge({credentials})
+	r.setCredentials = function (credentials) {
+		return this.merge({ credentials: credentials })
 	}
 
-	injectHook(f) {
-		let req = this.clone()
+	r.injectHook = function (f) {
+		var req = this.clone()
 		req.hooks.push(f)
 		return req
 	}
 
-	setPath(newpath) {
-		let req = this.clone()
+	r.setPath = function (newpath) {
+		var req = this.clone()
 		req.path = newpath
 		return makeUrl(req)
 	}
 
-	setHeader(headers) {
-		let req = this.clone()
+	r.setHeader = function (headers) {
+		var req = this.clone()
 		req.headers = Object.assign({}, this.headers, headers)
 		req.headers['Content-Type'] = undefined
 		return req
 	}
 
-	setMethod(method) {
-		let req = this.clone()
+	r.setMethod = function (method) {
+		var req = this.clone()
 		req.method = norm(method)
 		return req
 	}
 
-	setBase(base) {
-		let req = this.clone()
+	r.setBase = function (base) {
+		var req = this.clone()
 		req.base = norm(base)
 		return makeUrl(req)
 	}
 
-	setContentType(ty) {
-		return this.merge({content_type: norm(ty)})
+	r.setContentType = function (ty) {
+		return this.merge({ content_type: norm(ty) })
 	}
 
-	setParser(parser) {
-		let req = this.clone()
+	r.setParser = function (parser) {
+		var req = this.clone()
 		switch (norm(parser)) {
-			case 'json':
-				req.parse = _ => JSON.parse(_)
-				break
-			default:
-				req.parse = _ => _
-				break
+		case 'json':
+			req.parse = function (data) {
+				return JSON.parse(data)
+			}
+			break
+		default:
+			req.parse = asis
+			break
 		}
 		return req
 	}
 
-	send(data) {
-		let req = this.clone()
+	r.send = function (data) {
+		var req = this.clone()
 		if (data) {
-			if (this.content_type == 'json') {
+			if (this.content_type === 'json') {
 				req.body = JSON.stringify(data)
-			} else if (this.content_type == 'form') {
+			} else if (this.content_type === 'form') {
 				req.body = querystring.stringify(data)
 			} else {
 				req.body = data
 			}
 		}
-		req.url += req.query
+		req.url += (req.query || '')
 		return dosend(req)
 	}
+	return r
 }
 
-const serializeQuery = obj => {
-	var str = [];
-	for (var p in obj) if (obj.hasOwnProperty(p)) {
-		str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+var serializeQuery = function (obj) {
+	var str = []
+	for (var p in obj) {
+		if (obj.hasOwnProperty(p)) {
+			str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]))
+		}
 	}
-	return str.join("&");
+	return str.join('&')
 }
 
-const makeUrl = req => {
-	req.url = req.base + req.path
+var makeUrl = function (req) {
+	req.url = (req.base || '') + (req.path || '')
 	return req
 }
 
-const dosend = async req => {
-	req.content_type = getRealType(req.content_type)
-	if (req.content_type) {
-		req.headers = Object.assign(req.headers || {}, {
-			'Content-Type': req.content_type,
-		})
-	}
-	let resp
-	try {
-		resp = await env.fetch.bind(env.window)(req.url, req)
-	} catch (e) {
-		return [0, undefined, e]
-	}
-
-	let body = await resp.text()
-	let param = {
-		req,
-		code: resp.status,
-		body,
-	}
-	for (let f of req.hooks)
-		if (!await f(param)) break
-
-	body = param.body
-	let err;
-	try {
-		body = req.parse(body)
-	} catch (e) {
-		err = e
-	}
-	return [param.code, body, err]
+var dosend = function (req) {
+	return new Promise(function (rs, rj) {
+		req.content_type = getRealType(req.content_type)
+		if (req.content_type) {
+			req.headers = Object.assign(req.headers || {}, {
+				'Content-Type': req.content_type,
+			})
+		}
+		var resp
+		var param
+		env.fetch
+			.bind(env.window)(req.url, req)
+			.then(function (r) {
+				resp = r
+				return resp.text()
+			})
+			.then(function (body) {
+				param = { req: req, code: resp.status, body: body }
+				return waterfall(req.hooks, param)
+			})
+			.then(function () {
+				var body = param.body
+				try {
+					body = req.parse(body)
+					rs([param.code, body])
+				} catch (err) {
+					rs([undefined, undefined, err])
+				}
+			})
+			.catch(function (err) {
+				rs([0, undefined, err])
+			})
+	})
 }
 
-const norm = str => (str || "").trim()
+var norm = function (str) {
+	return (str || '').trim()
+}
 
-const getRealType = type => {
+var getRealType = function (type) {
 	type = norm(type)
 	switch (type) {
-		case 'json':
-			return 'application/json; charset=utf-8';
-		case 'form':
-			return 'application/x-www-form-urlencoded';
+	case 'json':
+		return 'application/json; charset=utf-8'
+	case 'form':
+		return 'application/x-www-form-urlencoded'
 	}
 	return type
 }
@@ -174,4 +202,54 @@ var env = {
 	fetch: {},
 	window: {},
 }
-module.exports = { post,del,head,patch,Request,env,get}
+module.exports = {
+	post: post,
+	del: del,
+	head: head,
+	patch: patch,
+	env: env,
+	get: get,
+}
+/*
+var p1 = function () {
+	return new Promise(function (r) {
+		console.log('call p1')
+		setTimeout(r, 1000, true)
+	})
+}
+var p2 = function () {
+	return new Promise(r => {
+		console.log('call p2')
+		setTimeout(r, 1000, false)
+	})
+}
+var p3 = function () {
+	return new Promise(r => {
+		console.log('call p3')
+		setTimeout(r, 1000, true)
+	})
+}
+var ps = [p1, p2, p3]
+*/
+
+function waterfall (ps, param) {
+	if (ps.length === 0) {
+		return new Promise(function (r) {
+			r(true)
+		})
+	}
+	var fp = ps.shift()
+	return fp(param).then(function (b) {
+		if (!b) {
+			return new Promise(function (r) {
+				r(false)
+			})
+		}
+		return waterfall(ps)
+	})
+}
+/*
+waterfall(ps).then(_ => {
+	console.log('ok')
+})
+*/
